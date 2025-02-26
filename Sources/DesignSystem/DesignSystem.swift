06d161fce9c1293d06ae2d283cffbffdf56af55d
avis001 <k.sivavel@gmail.com>
2025-02-26 17:17:55 +0530

    main - Remove unwanted NameSpace

--- Sources/DesignSystem/DesignSystem.swift
+++ Sources/DesignSystem/DesignSystem.swift
@@ -1,16 +1,14 @@
 import SwiftUI
 
-public enum DesignSystem {
-    public enum Spacing {
-        /// 4 points - Minimal spacing
-        public static let xs: CGFloat = 4
-        /// 8 points - Default spacing
-        public static let small: CGFloat = 8
-        /// 16 points - Medium spacing
-        public static let medium: CGFloat = 16
-        /// 24 points - Large spacing
-        public static let large: CGFloat = 24
-        /// 32 points - Extra large spacing
-        public static let xl: CGFloat = 32
-    }
+public enum Spacing {
+    /// 4 points - Minimal spacing
+    public static let xs: CGFloat = 4
+    /// 8 points - Default spacing
+    public static let small: CGFloat = 8
+    /// 16 points - Medium spacing
+    public static let medium: CGFloat = 16
+    /// 24 points - Large spacing
+    public static let large: CGFloat = 24
+    /// 32 points - Extra large spacing
+    public static let xl: CGFloat = 32
 }
